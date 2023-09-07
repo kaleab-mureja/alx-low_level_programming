a502daa845a365d5189992a37af8fe65a1cd8330
Kaleab Mureja <kaleabmureja123@gmail.com>
2023-09-08 06:34:44 +0900

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Useless function
+ * Description: prints a string to screen
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
