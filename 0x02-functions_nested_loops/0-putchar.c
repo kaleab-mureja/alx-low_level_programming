d51f63ce8a3a3cb02d8ada0825f81b63641c867b
Kaleab Mureja <kaleabmureja123@gmail.com>
2023-09-14 07:14:05 +0900

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * main - entry block
- * no argument
- * Return: 0
- **/
-
+ * main - Entry point
+ *
+ * Description: This is the main function that prints the "_putchar" string.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char ermias[] = "_putchar";
+    char ermias[] = "_putchar";
+    int c;
 
-	int c;
+    for (c = 0; c < 8; c++)
+    {
+        _putchar(ermias[c]);
+    }
 
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(ermias[c]);
-	}
-	_putchar('\n');
-	return (0);
+    _putchar('\n');
+    return (0);
 }
 
         
