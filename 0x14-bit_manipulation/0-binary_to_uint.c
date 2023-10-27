799013b34a8febead11d987a38a2ca9fd473afef
kaleab <kaleabmureja123@gmail.com>
2023-10-27 22:40:46 +0300

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,29 +2,65 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertable char
- */
+	* _pow - returns the base raised to a certain power
+	* @base: number
+	* @power: power of base to be raised  to
+	* Description: returns the base raised to a certain power
+	* Return: int
+*/
+int _pow(int base, int power)
+{
+	if (power == 0)
+		return (1);
+	return (base * _pow(base, power - 1));
+}
+
+/**
+	* _str_len - returns the length of a string
+	* @s: string to get length of
+	* Description: returns the length of a string
+	* Return: int
+*/
+int _str_len(const char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (1 + _str_len(&s[1]));
+}
+
+/**
+	* binary_to_uint - converts a binary number to an unsigned int
+	* @b: string containing binary number
+	* Description: converts a binary number to an unsigned int
+	* Return: unsigned int
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total, power;
-	int len;
+	int i;
+	int length;
+	unsigned int result = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len]; len++)
+	length = _str_len(b);
+	i = length - 1;
+
+	while (b[i] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] == '0' || b[i] == '1')
+		{
+			if (b[i] == '1')
+			{
+				result += 1 * _pow(2, ((length - 1) - i));
+			}
+			i--;
+		}
+		else
+		{
 			return (0);
+		}
 	}
 
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-	{
-		if (b[len] == '1')
-			total += power;
-	}
-
-	return (total);
+	return (result);
 }
